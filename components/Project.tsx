12d2eae2c6c72b666dff986b8f3d845e715ddeee
PKFireBarry <Barry0719@gmail.com>
2023-02-13 13:24:03 -0500

    Version 0.3.2
    Adding Clickable link to the photos for each project

--- components/Project.tsx
+++ components/Project.tsx
@@ -10,20 +10,24 @@ type Props = {
     photo: string;
 }
 
-function Project({title, description, stack, link, photo}: Props) {
+function Project({title, description, stack, link, photo, website}: Props) {
   return (
 
 <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 sm:p-24 min-h-screen min-w-fit h-screen'>
-
-    <motion.img
-    initial={{y: -300, opacity: 0}}
-    whileInView={{y: 0, opacity: 1}}
-    transition={{duration: 1}}
-    viewport={{ once: true }}
-    src={photo}
-    alt={title}
-    className='h-48 w-48 md:h-96 md:w-96 border rounded-md border-gray-500 object-cover xl:h-96 xl:w-96'
-    />
+    <a
+    className=''
+    href={website}
+    >
+      <motion.img
+      initial={{y: -300, opacity: 0}}
+      whileInView={{y: 0, opacity: 1}}
+      transition={{duration: 1}}
+      viewport={{ once: true }}
+      src={photo}
+      alt={title}
+      className='h-48 w-48 md:h-96 md:w-96 border rounded-md border-gray-500 object-cover xl:h-96 xl:w-96'
+      />
+    </a>
     <div className='space-y-10 px-0 md:px-10 max-w-4xl'>
         <h3 className='text-3xl font-semibold text-center'>
             <span className='underline '>Project: {title}</span>
