4673b3871cc59509aeb42cecebc009035eca924c
PKFireBarry <Barry0719@gmail.com>
2023-03-06 04:58:57 -0500

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -31,7 +31,7 @@ function Projects({}: Props) {
 
         </div>
 
-        <div className='absolute w-full top-[30%] bg-blue-300 left-0 h-[500px] -skew-y-12'></div>
+        <div className='absolute w-full top-[30%] bg-black left-0 h-[500px] -skew-y-12'></div>
     </motion.div>
   )
 }
